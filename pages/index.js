a23e33daa0e70f5f9a2d91794cd3cd252d55e4bd
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-11-01 02:36:34 -0300

    chore: add next, react and react-dom

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>Forum</h1>
+            <p>Here you write and evaluate everything that interests you</p>
+        </>
+    )
+}
+
+export default Home
